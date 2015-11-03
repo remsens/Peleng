4b56543527893ced7a7cfa5a22c9f513fe1e3525
Lina Shiryaeva <linashiryaeva@yandex.ru>
2015-11-03 12:29:01 +0300

    new class addSpectr

--- Peleng/Library/Attributes/Attributes.h
+++ Peleng/Library/Attributes/Attributes.h
@@ -6,6 +6,7 @@
 #include <QMap>
 #include <QString>
 #include <QList>
+#include <QVector>
 #include <../Library/PointStruct.h>
 
 
@@ -25,6 +26,14 @@ public:
     void ClearList();
     void SetAvailablePlugins(const QMap<QString, ProcessingPluginsInterface*> availablePlugins);
 
+
+    void SetModeLib(bool value);
+    bool GetModeLib() const;
+    const QVector<double>& GetXUnits() const;
+    const QVector<double>& GetYUnits() const;
+    const QMap<QString, QString>& GetDescriptionSpectr() const;
+
+
     const QList<Point>& GetPointsList() const;
     const QMap<QString, ProcessingPluginsInterface*>& GetAvailablePlugins() const;
 
@@ -39,6 +48,13 @@ private:
 private:
     QList<Point> m_pointsList;
     QMap<QString, ProcessingPluginsInterface*> m_availablePlugins;
+
+
+    bool m_addSpectr; //! 0 - создать новый, 1 - выгрузить из библиотеки
+    QVector<double> m_XUnits;
+    QVector<double> m_YUnits;
+    QMap<QString, QString> m_descriptionSpectr;
+
 };
 
 #endif // ATTRIBUTES_H
