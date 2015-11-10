919c85d8159d44e4d58aa4d1eee19bbec7bccf11
AntonMartenov <antonmartenov@gmail.com>
2015-11-10 15:04:18 +0300

    few updates with contrast on 3D

--- Peleng/3DHyperCube/ContrastWindow.cpp
+++ Peleng/3DHyperCube/ContrastWindow.cpp
@@ -13,8 +13,10 @@ ContrastWindow::ContrastWindow(int initMin, int initMax, QWidget *parent) :
 
     ui->sliderMin->setMinimum(MIN);
     ui->sliderMin->setMaximum(MAX);
+    ui->sliderMin->setValue(MIN);
     ui->sliderMax->setMinimum(MIN);
     ui->sliderMax->setMaximum(MAX);
+    ui->sliderMax->setValue(MAX);
     setAttribute(Qt::WA_DeleteOnClose);
     connect(ui->sliderMin,SIGNAL(valueChanged(int)),SLOT(minChngdSlot(int)));
     connect(ui->sliderMax,SIGNAL(valueChanged(int)),SLOT(maxChngdSlot(int)));
