1c5bd70e2b4ee280e77d4289659a6f91263f052d
AntonMartenov <antonmartenov@gmail.com>
2015-11-23 11:31:37 +0300

    just form for ui + few functions from resize cube

--- Peleng/3DHyperCube/mainwindow.h
+++ Peleng/3DHyperCube/mainwindow.h
@@ -5,7 +5,7 @@
 #include <QtWidgets>
 #include "../Library/HyperCube.h"
 #include "../Library/Attributes/Attributes.h"
-
+#include "GLWidget.h"
 namespace Ui {
 class MainWindow;
 }
@@ -20,18 +20,23 @@ public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
     void processData(HyperCube* ptrCube, Attributes* attr);
-
+    void resizeCube(u::uint32 Ch1, u::uint32 Ch2, u::uint32 R1, u::uint32 R2, u::uint32 C1, u::uint32 C2);
 
 public slots:
    void labelBright(int x, int y, QString brightValue);
    void showLabel_toggled(bool value);
-   //void labelLineHelp(QPoint mousePoint,QString label);
+   void cubeResized();
+   void prepareToResizeCube();
 protected:
     void closeEvent(QCloseEvent *) Q_DECL_OVERRIDE;
 
 
 private:
+    void setSlidersSettings();
+
     Ui::MainWindow *ui;
+    GLWidget *widgetHyperCube;
+    HyperCube* hyperCube;
     QLabel* pBrLabel;
 };
 
