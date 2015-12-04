ec68ef20207f6deb043638b99d38d7ca38a1498f
Lina Shiryaeva <linashiryaeva@yandex.ru>
2015-12-04 14:45:38 +0300

    trubles with release version

--- Peleng/MainApp/mainwindow.cpp
+++ Peleng/MainApp/mainwindow.cpp
@@ -109,6 +109,7 @@ void MainWindow::LoadFile()
             //m_pelengPlugin = m_pluginsControl->GetProcessingPlugins().value("Noise Remover");
             //m_pelengPlugin = m_pluginsControl->GetProcessingPlugins().value("Spectr UI");
             Attributes::I()->SetAvailablePlugins(m_pluginsControl->GetProcessingPlugins());
+            //Attributes::I()->SetPoint(150, 150, 200);
            // Attributes::I()->SetModeLib(1);
             cube->ResizeCube(0,223,50,1200,50,400);//чтобы не висла память
             m_pelengPlugin->Execute(cube, Attributes::I());
