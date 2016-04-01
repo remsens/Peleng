f1cd8ad67ce54ead84921cc847f1aecc736b8949
AntonMartenov <antonmartenov@gmail.com>
2016-04-01 15:47:52 +0300

    Ресайз при старте больше НЕ проводится

--- Peleng/MainApp/mainwindow.cpp
+++ Peleng/MainApp/mainwindow.cpp
@@ -97,13 +97,13 @@ void MainWindow::LoadFile()
             {
                 m_pelengPlugin = m_pluginsControl->GetProcessingPlugins().value("3DCube UI");
                 Attributes::I()->SetAvailablePlugins(m_pluginsControl->GetProcessingPlugins());
-                if(cube->GetLines() > 2300)
-                {
-                    if(cube->GetColumns() > 400)
-                        cube->ResizeCube(0,cube->GetCountofChannels()-1,1000,2200,50,400);//чтобы не висла память
-                    else
-                        cube->ResizeCube(0,cube->GetCountofChannels()-1,1000,2200,0,cube->GetColumns()-1);
-                }
+//                if(cube->GetLines() > 2300)
+//                {
+//                    if(cube->GetColumns() > 400)
+//                        cube->ResizeCube(0,cube->GetCountofChannels()-1,1000,2200,50,400);//чтобы не висла память
+//                    else
+//                        cube->ResizeCube(0,cube->GetCountofChannels()-1,1000,2200,0,cube->GetColumns()-1);
+//                }
                 m_pelengPlugin->Execute(cube, Attributes::I());
 //                Attributes::I()->SetMaskPixelsCount(3);
 //                Attributes::I()->ClearList();
