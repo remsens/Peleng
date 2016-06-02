0ca31937e2433465f24bc248106bd6242a76894f
AntonMartenov <antonmartenov@gmail.com>
2016-06-02 14:02:02 +0300

    some work

--- Peleng/3DHyperCube/ContrastWindow.h
+++ Peleng/3DHyperCube/ContrastWindow.h
@@ -20,11 +20,16 @@ signals:
 private slots:
     void minChngdSlot(int val);
     void maxChngdSlot(int val);
-
+//    void showMinLabel(int val);
+//    void showMaxLabel(int val);
+private:
+    int intFromDouble(double val);
+    double doubleFromInt(int val);
 private:
     Ui::ContrastWindow *ui;
     int MIN;
     int MAX;
+    int dpi = 100;
 };
 
 #endif // CONTRASTWINDOW_H
